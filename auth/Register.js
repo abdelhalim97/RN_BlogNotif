ee66b1a948773b09cbcc041840a207d1d237784a
abdelhalim97 <abdelhalimbenoun@gmail.com>
2021-12-10 20:21:52 +0100

    m

--- auth/Register.js
+++ auth/Register.js
@@ -1,116 +1,142 @@
 import React,{useState} from 'react'
-import { View, Text,StyleSheet,Image } from 'react-native'
-import { faCheck,faEye } from '@fortawesome/free-solid-svg-icons'
-import {BackGround,BarReset,HeaderCustom} from '../general'
+import { View, Text,StyleSheet,ImageBackground,KeyboardAvoidingView } from 'react-native'
+import {BarReset,HeaderCustom} from '../general'
 import { LabelInput,ButtonOpacity } from '../components'
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faFileSignature } from '@fortawesome/free-solid-svg-icons'
 import { useNavigation } from '@react-navigation/core';
-import { createUserWithEmailAndPassword, onAuthStateChanged,signOut } from '@firebase/auth'
+import { createUserWithEmailAndPassword } from '@firebase/auth'
 import {auth} from "../firebase"
-const imageGoogle ={uri:"https://img.icons8.com/color/96/000000/google-logo.png"}
+import { useDispatch } from 'react-redux'
+import {setUserRedux} from "../slices/CounterSlices"
+import GoogleAuth from './GoogleAuth'
+import firebase from 'firebase';
 
 export default function Register() {
+    const image ={uri:"https://images.unsplash.com/photo-1518976024611-28bf4b48222e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=385&q=80"}
+    const imageUser ={uri:"https://firebasestorage.googleapis.com/v0/b/rnblog-d20d4.appspot.com/o/images%2Fimage404.png?alt=media&token=6b03e815-306f-4cd2-97f3-1a54d999879c"}
+    const dispatch = useDispatch()
     const [registerEmail, setRegisterEmail] = useState("")
     const [registerPassword, setRegisterPassword] = useState("")
+    const [registerDisplayName, setDisplayName] = useState("")
+    const [registerImageURL, setImageURL] = useState("")
     const navigation=useNavigation();
-    const [userFirstTime, setUserFirstTime] = useState({})
+    // const register =  ()=>{
+    //     auth
+    //     .createUserWithEmailAndPassword(registerEmail,registerPassword)
+    //     .then((authUser)=>{
+    //         authUser.user.updateProfile({
+    //             displayName: registerDisplayName,
+    //             photoURL:imageUser||registerImageURL
+    //         });
+    //         // dispatch(setUserRedux({
+    //         //     email:registerEmail,
+    //         //     pass:registerPassword,
+    //         //     displayName: registerDisplayName,
+    //         //     image:registerImageURL.trim().length>0?registerImageURL:imageUser,
+    //         // }))
+    //         // navigation.navigate("Accueil")
+    //     })
+    //     .catch((error)=>alert(error.message))
+    // }
     const register = async ()=>{
         try {
-        const user = await createUserWithEmailAndPassword(auth,registerEmail,registerPassword);
+        const user = await createUserWithEmailAndPassword(auth,registerEmail,registerPassword)
+        await firebase.auth().currentUser.updateProfile({
+            displayName: registerDisplayName,
+            photoURL: "",
+          });
+        dispatch(setUserRedux({
+            email:registerEmail,
+            pass:registerPassword}))
         navigation.navigate("Accueil")
         } catch (error) {
             console.log(error)
         }
     }
-    onAuthStateChanged(auth,(currentUser)=>{
-        setUserFirstTime(currentUser)
-    })
-    // const register=()=>{
-    //     auth.createUserWithEmailAndPassword(email,password)
-    //     .then((authUser) =>{
-    //         authUser.user.update({
-    //             displayName:name,
-    //         // photoURL:imageUrl||"https://img.icons8.com/color/96/000000/google-logo.png"
-    //     })}).
-    //     catch((error)=>alert(error.message))
-    // }
     const btn =[
         {
             id:1,
-            name:"Sign Up",
+            name:"S'inscrire",
             fnc:register,
         },
     ]
     const data =[
         {
-          id:1,
-          label:"Email",
-          placeH:"Type your Email here",
-          icon:faCheck,
-          iconS:20,
+            id:1,
+            label:"Nom",
+            pass:false,
+            value:registerDisplayName,
+          },
+        {
+          id:2,
+          label:"E-mail",
           pass:false,
           value:registerEmail,
         },
+        
         {
-          id:2,
-          label:"Password",
-          placeH:"Type your pass here",
-          icon:faEye,
-          iconS:20,
+          id:3,
+          label:"Mot de passe",
           pass:true,
           value:registerPassword,
-        }
+        },
+        {
+            id:4,
+            label:"Image de Profile(Option)",
+            pass:false,
+            value:registerImageURL,
+          },
       ]
     return (
-        <>
-            <BackGround></BackGround>
-            <View style={{ flex:1,position:"absolute",width:"100%",height:"100%"}}>
+        <ImageBackground source={image} resizeMode="cover" style={{height:'100%' }}>
+      <View style={{backgroundColor: "#dedace",opacity:0.86,height:'100%'}}></View>
+            <View style={{ position:"absolute",width:"100%",height:"100%"}}>
                 <BarReset></BarReset>
                 <HeaderCustom></HeaderCustom>
-                <View style={styles.LogIn}>
+                <KeyboardAvoidingView keyboardVerticalOffset={-270}
+                behavior={ "position"}
+                contentContainerStyle={styles.LogIn}>
                     <View style={{ justifyContent:"flex-start",flexDirection:"row" }}>
-                        <FontAwesomeIcon icon={faFileSignature} size={25} style={{color:"#3A2298",marginTop:9}}/>
-                        <Text style={{ color:"#3A2298",alignItems:"center",paddingLeft:5 }}>Register</Text>
+                        <Text style={{ color:"#41928D",alignItems:"center",paddingLeft:5,fontSize:28 }}>S'inscrire</Text>
+                    </View>
+                    <View style={{ alignItems:"center",marginTop:14 }}>
+                        <GoogleAuth></GoogleAuth>
                     </View>
                     <View style={{ marginTop:20 }}>
                     {
                         data.map((d)=>
                         <LabelInput key={d.id} icon={d.icon} placeH={d.placeH} label={d.label}
-                        iconS={d.iconS} pass={d.pass} 
-                        value={d.value}
-                        onChangeText={text => d.id===1?setRegisterEmail(text):setRegisterPassword(text)}>
+                        iconS={d.iconS} pass={d.pass} value={d.value}
+                        onChangeText={text => d.id===1?setDisplayName(text):
+                        d.id===2?setRegisterEmail(text):
+                        d.id===3?setRegisterPassword(text):
+                        setImageURL(text)}>
                         </LabelInput>)
                     }
                     </View>
                     <View style={{ justifyContent:"space-around",marginTop:25,flexDirection: "row"}}>
                     {
                         btn.map((data)=>
-                        <ButtonOpacity key={data.id} fnc={data.fnc} name={data.name}></ButtonOpacity>)
+                        <ButtonOpacity key={data.id} fnc={data.fnc} name={data.name}
+                        disabled={registerEmail.trim().length>0 &&
+                            registerPassword.trim().length>0&&
+                            registerDisplayName.trim().length>0?false:true}></ButtonOpacity>)
                     }
                     </View>
-                    <View style={{ alignItems:"center",marginTop:14 }}>
-                        <Image source={imageGoogle} style={{height:60,width:60}}/>
-                    </View>
-                    <View><Text>{userFirstTime?.email}</Text></View>
-                    <ButtonOpacity fnc={()=>signOut(auth)}  name='sign out'></ButtonOpacity>
-                </View>
+                </KeyboardAvoidingView>
             </View>
-            <Text>
-            </Text>
-        </>
+        </ImageBackground>
     )
 }
 const styles = StyleSheet.create({
     LogIn: {
-      backgroundColor:"#FFF",
-      flex:1,
-      marginTop:70,
-      paddingLeft:10,
-      paddingRight:10,
-      borderWidth: 1,
-      borderColor:"#FFF",
-      borderTopLeftRadius:20,
-      borderTopRightRadius:20,
+        backgroundColor:"#FFF",
+        marginTop:70,
+        paddingLeft:25,
+        paddingRight:25,
+        borderWidth: 1,
+        borderColor:"#FFF",
+        borderTopLeftRadius:20,
+        borderTopRightRadius:20,
+        height: "100%"
     },
   });
